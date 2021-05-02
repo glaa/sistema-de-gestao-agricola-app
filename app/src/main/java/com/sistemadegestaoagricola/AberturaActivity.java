c17d9ac656fd7fdf718167543428756c3004597a
Gleison A. Araujo <gleisonaaraujo@gmail.com>
2021-05-02 10:54:31 -0300

    Criado Splash Screen

---
+++ app/src/main/java/com/sistemadegestaoagricola/AberturaActivity.java
@@ -0,0 +1,35 @@
+package com.sistemadegestaoagricola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class AberturaActivity extends AppCompatActivity implements Runnable{
+
+    Thread thread;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abertura);
+
+        handler = new Handler();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(20000);
+        } catch (Exception e){
+
+        }
+        finish();
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+}
